ca79fc290acbe365f33b5d9c36b42802eac873b3
devopslayer <layerdevops@gmail.com>
2025-05-14 23:00:55 +0530

    Enhance UI: Add favicon to index.html and include brand logo in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,19 @@
+import investmentLogo from "../../assets/investment.png";
 import "./Header.css";
 
 const Header = () => (
   <nav className="navbar navbar-dark bg-dark px-3">
-    <span className="navbar-brand mb-0 h1">Portfolio Dashboard</span>
+    <span className="navbar-brand mb-0 h1 d-flex align-items-center">
+      {/* Portfolio icons created by Eucalyp - Flaticon, For more details you can visit to (https://www.flaticon.com/free-icons/portfolio) */}
+      <img
+        src={investmentLogo}
+        alt="Brand Logo"
+        width="30"
+        height="30"
+        className="d-inline-block align-top me-3"
+      />
+      Portfolio Dashboard
+    </span>
   </nav>
 );
 
