c777adba77e06fbd43bd304a39513a60d865a744
devopslayer <layerdevops@gmail.com>
2025-05-17 08:40:10 +0530

    fix: Check routing base url

--- src/services/financeAPI.js
+++ src/services/financeAPI.js
@@ -1,6 +1,8 @@
 export const fetchPortfolioData = async () => {
   try {
-    const response = await fetch("/mock-data/portfolio.json");
+    const response = await fetch(
+      `${import.meta.env.BASE_URL}mock-data/portfolio.json`
+    );
     if (!response.ok) {
       throw new Error("Failed to load portfolio data");
     }
